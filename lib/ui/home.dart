a9e50d74f29ed637ebeaf843b2fad8c192ca03be
Dinesh <s.ydinesh1177@gmail.com>
2019-07-14 16:28:27 +0530

    Initial Commit

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,11 +5,12 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-       appBar: new AppBar(
-         title: Text("NoToDo"),
-         backgroundColor: Colors.black54,
-       ),
+      appBar: new AppBar(
+        title: Text("NoToDo"),
+        backgroundColor: Colors.black54,
+      ),
       body: new NotoDoScreen(),
     );
+
   }
 }
